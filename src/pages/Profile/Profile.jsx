a3eb1abaedc69f2789ec531e85ff38ba033c0bb3
Priyobrata Mondal <priyobrata.2212@gmail.com>
2024-03-27 14:42:01 +0530

    Updated Profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import "./Profile.css";
+import { Container } from "react-bootstrap";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -10,11 +12,19 @@ const Profile = () => {
 
   return (
     isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
+      <Container className="section">
+        <div className="user-details-container">
+          <img
+            src={user.picture}
+            alt={user.name}
+            className="user-img"
+            height={200}
+            width={200}
+          />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      </Container>
     )
   );
 };
