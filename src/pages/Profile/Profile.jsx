aa4f55b16856d767af8799f311ea8073208affc0
Anirban Bandyopadhyay <136166021+anirban-git12@users.noreply.github.com>
2024-03-27 19:24:07 +0530

    commit

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import "./Profile.css";
 import { Container } from "react-bootstrap";
+import img1 from '../../utils/images/about-page-header.jpg'
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -11,25 +12,31 @@ const Profile = () => {
   }
 
   return (
-    
-    
     isAuthenticated && (
-      <>
-      <section className="section"></section>
-      
-        <div className="user-details-container">
+      <div>
+        <div className="header">
+          {/* Place your header image here */}
           <img
-            src={user.picture}
-            alt={user.name}
-            className="user-img"
-            height={200}
-            width={200}
+            src={img1}
+            alt="Header Image"
+            className="header-img"
           />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-        
+        </div>
+        <div className="section">
+          <div className="user-details-container">
+            <h1>PROFILE</h1>
+            <img
+              src={user.picture}
+              alt={user.name}
+              className="user-img"
+              height={200}
+              width={200}
+            />
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+        </div>
       </div>
-        </>
     )
   );
 };
