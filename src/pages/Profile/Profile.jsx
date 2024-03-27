7a6d53929482ddb6ab51f1ba2dcdd834a565e935
SUJAY DEY <149602382+SujayDeyTMSL@users.noreply.github.com>
2024-03-27 18:41:20 +0530

    profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -27,6 +27,25 @@ const Profile = () => {
           />
           <h2>{user.name}</h2>
           <p>{user.email}</p>
+          <div className="container1">
+            <div class="row">
+          
+          <div class="col-12 col-sm-6 col-lg-3">
+            <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+            
+              <div class="advisor_thumb"><img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" />
+                
+                <div class="social-info"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
+              </div>
+              
+              <div class="single_advisor_details_info">
+                <h6>Samantha Sarah</h6>
+                <p class="designation">Founder &amp; CEO</p>
+              </div>
+            </div>
+            </div>
+          </div>
+          </div>
         
       </div>
         </>
